92b18719229b88731fbd9beb7ec57eb4fc5fa912
Nikhil Hade <nikhilhade21@gmail.com>
2024-01-01 20:28:57 +0530

    vaxsd

--- public/js file/style-switcher1.js
+++ public/js file/style-switcher1.js
@@ -28,9 +28,9 @@ function setActiveStyle(color)
 /*====================================== theme light and dark =====================*/
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun")
+    dayNight.querySelector("i").classList.toggle("fa-sun")  
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("dark"); 
 })
 window.addEventListener("load",() => {
     if(document.body.classList.contains("dark"))
